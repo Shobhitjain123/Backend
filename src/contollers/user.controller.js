5188e50ca4282b61868f6488d1258b148e6fba13
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-08-18 11:54:50 +0530

    Test Change

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -415,7 +415,6 @@ const getUserProfileDetails = asyncHandler(async (req, res) => {
   if(!channel?.length){
     throw new ApiError(404, "Channel does not exist")
   }
-
   return res.status(200).json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
 });
 
